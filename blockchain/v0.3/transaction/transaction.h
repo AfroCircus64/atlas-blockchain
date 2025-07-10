9e004148097c23ddbfc7e53425a4a016ca9edf79
Trey Turner <trey.turner1857@gmail.com>
2025-07-09 23:58:00 -0500

    Task 1

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -110,6 +110,8 @@ typedef struct tx_data_s
 } tx_data_t;
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
+     uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
 
 
 #endif /* TRANSACTION_H */
---
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -0,0 +1,31 @@
+#include "transaction.h"
+
+/**
+ * unspent_tx_out_create - Creates an unspent transaction output
+ *
+ * @block_hash: Hash of the Block containing the transaction
+ * @tx_id: ID of the transaction containing the output
+ * @out: Pointer to the transaction output
+ *
+ * Return: Pointer to unspent transaction output, or NULL on failure
+ */
+unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	 uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
+{
+	unspent_tx_out_t *unspent = malloc(sizeof(unspent_tx_out_t));
+
+	if (!unspent)
+	{
+		return (NULL);
+	}
+
+	memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+
+	if (out)
+	{
+		unspent->out = *out;
+	}
+
+	return (unspent);
+}
